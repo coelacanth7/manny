702bd0b1545866a04876fbe04ad7cba3c1728b8a
Coelacanth7 <galford151@gmail.com>
2018-12-22 18:25:33 -0500

    react swipe werks

--- src/ui/Home.js
+++ src/ui/Home.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => {
-	return (
-		<div className="pane">
-			<h3>ABOUT</h3>
-			<h3>GIGS</h3>
-			<h3>LINKS</h3>
-			<h3>ART</h3>
-		</div>
-	);
-};
+class Home extends Component {
+	constructor(props) {
+		super(props);
+
+		// this.next = this.next.bind(this);
+		// this.prev = this.prev.bind(this);
+		this.clickContent = this.clickContent.bind(this);
+		this.tableOfContents = this.tableOfContents.bind(this);
+	}
+
+	contents = ["ABOUT", "GIGS", "LINKS", "ART"];
+
+	clickContent(e) {
+		this.props.onClickNode(e.target.value);
+	}
+
+	tableOfContents() {
+		return this.contents.map((el, i) => (
+			<h3 onClick={this.clickContent} key={i} value={i}>
+				{el}
+			</h3>
+		));
+	}
+
+	render() {
+		return <div className="pane">{this.tableOfContents()}</div>;
+	}
+}
 
 export default Home;
--- src/ui/UI.js
+++ src/ui/UI.js
@@ -25,6 +25,7 @@ class UI extends Component {
 		this.prev = this.prev.bind(this);
 		this.onClickNode = this.onClickNode.bind(this);
 		this.renderPaneNodes = this.renderPaneNodes.bind(this);
+		this.getPosition = this.getPosition.bind(this);
 	}
 
 	next() {
@@ -39,6 +40,10 @@ class UI extends Component {
 		this.reactSwipe.slide(num);
 	}
 
+	getPosition() {
+		this.reactSwipe.getPos();
+	}
+
 	renderPaneNodes() {
 		return this.nodes.map((node, i) => (
 			<div key={i}>
@@ -47,24 +52,41 @@ class UI extends Component {
 		));
 	}
 
-  nodes = [
-  	<Home />,
-  	<About />,
-  	<Gigs />,
-  	<Art />,
-  	<Links />
-  ];
+	nodes = [
+		// <Home onClickNode={this.onClickNode} />,
+		<div className="pane">
+			<h3 onClick={() => this.onClickNode(1)}>ABOUT</h3>
+			<h3 onClick={() => this.onClickNode(2)}>GIGS</h3>
+			<h3 onClick={() => this.onClickNode(3)}>LINKS</h3>
+			<h3 onClick={() => this.onClickNode(4)}>ART</h3>
+		</div>,
+		<About />,
+		<Gigs />,
+		<Links />,
+		<Art />
+	];
+
+	header = (
+		<div>
+			<span onClick={() => this.onClickNode(0)}>HOME</span>
+			<span onClick={() => this.onClickNode(1)}>ABOUT</span>
+			<span onClick={() => this.onClickNode(2)}>GIGS</span>
+			<span onClick={() => this.onClickNode(3)}>LINKS</span>
+			<span onClick={() => this.onClickNode(4)}>ART</span>
+		</div>
+	);
 
 	render() {
 		return (
 			<div className="center">
-				{/* <button onClick={() => this.onClickNode(2)}>2</button> */}
+				{/* <button onClick={() => this.onClickNode(1)}>2</button> */}
 				<ReactSwipe
 					ref={reactSwipe => (this.reactSwipe = reactSwipe)}
 					className="mySwipe"
 					swipeOptions={swipeOptions}
 				>
-          {this.renderPaneNodes()}
+					{/* <div>{this.getPosition()}</div> */}
+					{this.renderPaneNodes()}
 				</ReactSwipe>
 
 				<div style={{ textAlign: "center" }}>
@@ -79,4 +101,5 @@ class UI extends Component {
 		);
 	}
 }
+
 export default UI;
