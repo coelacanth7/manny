29996e7497bffa28faa05eafe452624a2074323e
Coelacanth7 <galford151@gmail.com>
2018-12-22 20:39:05 -0500

    added functional header

--- src/ui/UI.js
+++ src/ui/UI.js
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
 import ReactSwipe from "react-swipe";
 
-import Home from "./Home";
 import About from "./About";
 import Gigs from "./Gigs";
 import Links from "./Links";
 import Art from "./Art";
 
-// change Swipe.js options by query params
-const swipeOptions = {
-	callback: function callback() {
-		console.log("slide changed");
-	},
-	transitionEnd: function transitionEnd() {
-		console.log("ended transition");
-	}
-};
-
 class UI extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			pos: 0
+		};
 
 		this.next = this.next.bind(this);
 		this.prev = this.prev.bind(this);
 		this.onClickNode = this.onClickNode.bind(this);
 		this.renderPaneNodes = this.renderPaneNodes.bind(this);
-		this.getPosition = this.getPosition.bind(this);
 	}
 
 	next() {
@@ -38,10 +30,7 @@ class UI extends Component {
 
 	onClickNode(num) {
 		this.reactSwipe.slide(num);
-	}
-
-	getPosition() {
-		this.reactSwipe.getPos();
+		this.setState({ pos: this.reactSwipe.getPos() });
 	}
 
 	renderPaneNodes() {
@@ -53,7 +42,6 @@ class UI extends Component {
 	}
 
 	nodes = [
-		// <Home onClickNode={this.onClickNode} />,
 		<div className="pane">
 			<h3 onClick={() => this.onClickNode(1)}>ABOUT</h3>
 			<h3 onClick={() => this.onClickNode(2)}>GIGS</h3>
@@ -68,35 +56,37 @@ class UI extends Component {
 
 	header = (
 		<div>
-			<span onClick={() => this.onClickNode(0)}>HOME</span>
-			<span onClick={() => this.onClickNode(1)}>ABOUT</span>
-			<span onClick={() => this.onClickNode(2)}>GIGS</span>
-			<span onClick={() => this.onClickNode(3)}>LINKS</span>
-			<span onClick={() => this.onClickNode(4)}>ART</span>
+			<span onClick={() => this.onClickNode(0)}>HOME / </span>
+			<span onClick={() => this.onClickNode(1)}>ABOUT / </span>
+			<span onClick={() => this.onClickNode(2)}>GIGS / </span>
+			<span onClick={() => this.onClickNode(3)}>LINKS / </span>
+			<span onClick={() => this.onClickNode(4)}>ART / </span>
 		</div>
 	);
+
+	// change Swipe.js options by query params
+	swipeOptions = {
+		callback: () => {
+			this.setState({ pos: this.reactSwipe.swipe.getPos() });
+		},
+		transitionEnd: function transitionEnd() {
+			console.log("ended transition");
+		},
+		continuous: false
+	};
 
 	render() {
 		return (
 			<div className="center">
-				{/* <button onClick={() => this.onClickNode(1)}>2</button> */}
+				{this.state.pos !== 0 ? this.header : <div className="xt-space" />}
 				<ReactSwipe
 					ref={reactSwipe => (this.reactSwipe = reactSwipe)}
 					className="mySwipe"
-					swipeOptions={swipeOptions}
+					swipeOptions={this.swipeOptions}
+					id="slider"
 				>
-					{/* <div>{this.getPosition()}</div> */}
 					{this.renderPaneNodes()}
 				</ReactSwipe>
-
-				<div style={{ textAlign: "center" }}>
-					<button type="button" onClick={this.prev}>
-						Prev
-					</button>
-					<button type="button" onClick={this.next}>
-						Next
-					</button>
-				</div>
 			</div>
 		);
 	}
